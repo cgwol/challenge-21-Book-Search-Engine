8ec5335452153b1e24fc1d389ffe2ebf9cbe8cff
cgwol <cgwalterson@icloud.com>
2023-11-15 01:21:50 -0700

    server.js and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+
+type Auth{
+    token: ID!
+    user: User
+} 
+
+type Book{
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type User{
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Query {
+    loggedInUser: User
+}
+
+input SaveBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: ID
+    image: String
+    link: String
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(saveBook:SaveBook): User
+    removeBook(bookId: ID): User
+}
+
+`
+
+module.exports = typeDefs;
